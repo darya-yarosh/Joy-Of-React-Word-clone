1416476e64c64118e95b5c32ca8e04cf2ec5cda1
darya-yarosh <gurimusagi@yandex.ru>
2023-04-09 13:49:51 +0300

    Exercise 3: Added guess slots

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
+
+import "./GuessResults.css";
 
 function GuessResults({
   guessList
 }) {
+  const tableRows = range(NUM_OF_GUESSES_ALLOWED);
+  const tableCells = range(NUM_OF_GUESSES_ALLOWED-1);
+
+  const visibleGuessList = []
+  if (guessList.length !== 0) {
+    guessList.forEach((element, index) => {
+      visibleGuessList[index] = element;
+    });
+  }
 
   return <div className="guess-results">
-    {guessList.map((guess, index) =>
-      <div
-        key={index}
+    {tableRows.map((row) =>
+      <p
+        key={row}
         className="guess">
-          {guess}
-      </div>
+        {tableCells.map((cell) => 
+          <span className="cell" key={cell}>
+            {(visibleGuessList[row]!==undefined) && 
+            visibleGuessList[row][cell]
+            }
+          </span>
+        )}
+      </p>
     )}
   </div>;
 }
